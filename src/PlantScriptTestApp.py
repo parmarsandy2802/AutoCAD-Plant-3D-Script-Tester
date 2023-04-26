7843811bbd9d06b5df1a0d98f1c6b716756b5436
parmarsandy2802 <131883071+parmarsandy2802@users.noreply.github.com>
2023-04-26 19:03:56 +0530

    Update PlantScriptTestApp.py

--- src/PlantScriptTestApp.py
+++ src/PlantScriptTestApp.py
@@ -1,136 +1,23 @@
-# This tool was created for debugging Plant 3D part scripts during developement. It makes it possible to check for script errors without
-# running Plant 3D, registering scripts and restarting it over and over again.
-# Comment all the imports in your script file, then import * from this file to your script and invoke script function at the end of the
-# script to check for errors. If no problems were found, continue with checking your script in Plant 3D.
-# Author: Andrzej Lach
-from PlantScriptTest import *
-from ScriptReader import *
-from Settings import *
-import tkinter as tk
-from tkinter import filedialog as tkf
-import os
+from varmain.primitiv import *
+from varmain.custom import *
+from math import *
 
+@activate(Group="Support", TooltipShort="JD01", TooltipLong="JD01", LengthUnit="mm", Ports="1")
+@group("MainDimensions")
+@param(D=LENGTH, TooltipShort="Pipe Diameter", TootltipLong="Pipe Diameter")
+@param(TH=LENGTH, TooltipShort="Height", TooltipLong="Overall Height")
+@param(PL=LENGTH, TooltipShort="Pad Length", TooltipLong="Pad Length")
+@param(PW=LENGTH, TooltipShort="Pad Width", TooltipLong="Pad Width")
+@param(PT=LENGTH, TooltipShort="Pad Thickness", TooltipLong="Pad Thickness")
+@param(CSD=LENGTH, TooltipShort="Support Diameter", TooltipLong="Cylindrical Pipe Support Diameter")
+@param(CSH=LENGTH, TooltipShort="Support Height", TooltipLong="Cylindrical Pipe Height")
 
-class App:
-    def __init__(self, root):
-        self.clearConsole()
-        self.root = root
-        self.root.title("PlantScriptTest App")
-        self.path = tk.StringVar()
-        self.showScriptVar = tk.IntVar()
-        self.updateVar()
-        self.mainFrame = tk.Frame(self.root, bg="black")
-        self.mainFrame.pack()
-        self.pathFrame = tk.Frame(self.mainFrame, bg="black")
-        self.pathFrame.pack()
-        self.pathLabel = tk.Label(self.pathFrame, textvariable=self.path, width=100, fg="green2", bg="black")
-        self.pathLabel.pack(side='left', fill='x')
-        self.pathButton = tk.Button(self.pathFrame, text="Browse", command=self.getPath, bg="green", fg="white", activebackground='green3', activeforeground='black')
-        self.pathButton.pack(side="right")
-        self.subFrame = tk.Frame(self.mainFrame)
-        self.subFrame.pack(fill='x', expand=True)
-        self.clearButton = tk.Button(self.subFrame, text="CLEAR CONSOLE", command=self.clearConsole, state="disabled", bg="green", fg="white", activebackground='green3', activeforeground='black')
-        self.clearButton.pack(fill='x', side='right', expand=True)
-        self.showScriptButton = tk.Button(self.subFrame, text="SHOW SCRIPT BODY", command=self.showBody, state="normal", bg="black", fg="green", activebackground='green3', activeforeground='black')
-        self.showScriptButton.pack(side='left',fill='both', expand=True)
-        self.runButton = tk.Button(self.mainFrame, text="TEST SCRIPT", command=self.runTest, state="disabled", bg="green", fg="white", activebackground='green3', activeforeground='black')
-        self.runButton.pack(fill='x')
-        self.updateButton()
-        self.root.resizable(False, False)
-        #self.root.iconphoto(False, tk.PhotoImage(file='icon.png'))
-        self.root.mainloop()
-    
-    def showBody(self):
-        if self.showScriptVar.get() == 1:
-            self.showScriptVar.set(0)
-        else:
-            self.showScriptVar.set(1)
-        self.updateButton()
-        self.updateSettings()
-        
-    def updateButton(self):
-        if self.showScriptVar.get() == 1:
-            self.showScriptButton.config(bg="green3", fg="black", relief="sunken")
-        else:
-            self.showScriptButton.config(bg="green", fg="white", relief="raised")
-            
-    
-    def updateVar(self):
-        if Settings.ShowScriptBody:
-            self.showScriptVar.set(1)
-        else:
-            self.showScriptVar.set(0)
-            
-    def updateSettings(self):
-        if self.showScriptVar.get() == 1:
-            Settings.ShowScriptBody = True
-        else:
-            Settings.ShowScriptBody = False
-    
-    def clearConsole(self, *args):
-        os.system('cls')
-    
-    def getPath(self):
-        path = tkf.askopenfilenames(filetypes=[('Python file', 'py'), ('Python file', 'pyc_dis')])
-        if len(path) < 1:
-            return
-        elif path == "" or path == " " or path == None:
-            return
-        else:
-            if len(path) == 1:
-                self.path.set(path[0])
-                self.multi = False
-            else:
-                self.path.set("MULTIPLE FILES")
-                self.multi = True
-            self.paths = []
-            for i in path:
-                self.paths.append(i)
-        self.runButton.config(state="normal")
-        self.clearButton.config(state="normal")
-    
-    def runTest(self):
-        if len(self.paths) < 1:
-            return
-        elif len(self.paths) == 1:
-            self.runSingleTest()
-        elif len(self.paths) > 1:
-            self.runMultipleTests()
-        
-    def runSingleTest(self):
-        if self.path.get() == "" or self.path.get() == " " or self.path.get() == None:
-            return
-        if os.path.exists(self.path.get()):
-            ScriptReader(self.path.get())
-            self.resetID()
-        
-    def runMultipleTests(self):
-        for i in self.paths:
-            self.path.set(i)
-            self.runSingleTest()
-            
-    def resetID(self):
-        classes = [PLANTOBJECT,
-            MODIFIED_OBJECT,
-            CYLINDER,
-            TORUS,
-            ARC3D,
-            ARC3D2,
-            ARC3DS,
-            BOX,
-            CONE,
-            ELLIPSOIDHEAD,
-            ELLIPSOIDHEAD2,
-            ELLIPSOIDSEGMENT,
-            HALFSPHERE,
-            PYRAMID,
-            ROUNDRECT,
-            SPHERESEGMENT,
-            TORISPHERICHEAD,
-            TORISPHERICHEAD2,
-            TORISPHERICHEADH]
-        for i in classes:
-            i.ID = 1
-            
-root = tk.Tk()
-app = App(root)
+def JD01(s, D=60.00, TH=500.00, PL=100.00, PW=100.00, PT=10.00, CSD=60.00, CSH=450.00, ID="JD01", **kw):
+
+    # create Base Plate
+    oBP = BOX(s, L=PL, W=PW, H=PT).Translate(0,0,(PT/2.00)-TH)
+
+    # create cylindrical support on Base Plate
+    oCS = CYLINDER(s, R=CSD/2.00, H=CSH).rotateZ(90).translate(0,0,-TH)
+    oBP.uniteWith(oCS)
+    oCS.erase()
